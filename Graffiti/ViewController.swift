eafa4155bba1674d5bbae3c7073f1856d79b2292
Varun <sayalvarun@gmail.com>
2018-05-12 17:09:18 -0400

    initial xcode commits

--- Graffiti/ViewController.swift
+++ Graffiti/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
+class ViewController: UIViewController, ARSCNViewDelegate, UITextFieldDelegate {
+    
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    @IBOutlet weak var newTextBar: UITextField!
+    
+    @IBOutlet weak var addNewTextButton: UIButton!
+    
+    var planeNodes: Array<SCNNode> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +30,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        self.newTextBar.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,8 +39,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
+        // Vertical and horizontal plane detection
+        configuration.planeDetection = [.vertical, .horizontal]
+        
         // Run the view's session
         sceneView.session.run(configuration)
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -51,8 +58,77 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
+    
+    @IBAction func addNewTextTrigger(_ sender: Any) {
+        newTextBar.isHidden = false
+        newTextBar.becomeFirstResponder()
+    }
+    
+    // MARK: - UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == newTextBar {
+            textField.resignFirstResponder()
+            textField.isHidden = true
+            for plane in planeNodes {
+                let text = SCNText(string: textField.text, extrusionDepth: 0.0)
+                //ExtrusionDepth 0 => 2D
+                text.firstMaterial?.diffuse.contents = UIColor.white
+                text.font = UIFont(name: "Arial", size: 0.5)
+                let textNode = SCNNode(geometry: text)
+                let anchor = sceneView.anchor(for: plane) as? ARPlaneAnchor
+                textNode.opacity = 1.0
+                textNode.eulerAngles.x = -.pi / 2
+                textNode.position = SCNVector3Make(
+                    anchor!.center.x - (textNode.boundingBox.max.x/2),
+                    0.0,
+                    anchor!.center.z)
+                plane.addChildNode(textNode)
+                textField.text = ""
+            }
+            return false
+        }
+        return true
+    }
 
     // MARK: - ARSCNViewDelegate
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // Place content only for anchors found by plane detection.
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        // Create a SceneKit plane to visualize the plane anchor using its position and extent.
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        
+        // `SCNPlane` is vertically oriented in its local coordinate space, so
+        // rotate the plane to match the horizontal orientation of `ARPlaneAnchor`.
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        // Make the plane visualization semitransparent to clearly show real-world placement.
+        planeNode.opacity = 0.25
+        
+        // Add the plane visualization to the ARKit-managed node so that it tracks
+        // changes in the plane anchor as plane estimation continues.
+        node.addChildNode(planeNode)
+        planeNodes.append(node)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        // Update content only for plane anchors and nodes matching the setup created in `renderer(_:didAdd:for:)`.
+        guard let planeAnchor = anchor as?  ARPlaneAnchor,
+            let planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane
+            else { return }
+        
+        // Plane estimation may shift the center of a plane relative to its anchor's transform.
+        planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        
+        // Plane estimation may also extend planes, or remove one plane to merge its extent into another.
+        plane.width = CGFloat(planeAnchor.extent.x)
+        plane.height = CGFloat(planeAnchor.extent.z)
+    }
     
 /*
     // Override to create and configure nodes for anchors added to the view's session.
